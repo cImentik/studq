fc968320af9f3f588e2318c68b8c4de6379af608
cIment <ivan_bit_null@hotmail.com>
2014-05-19 21:20:15 +0800

    Определены  unicode “представления” объектов

--- quiz/models.py
+++ quiz/models.py
@@ -7,11 +7,20 @@ class Staff(models.Model):
     name = models.CharField(max_length=50)
     unit = models.CharField(max_length=10)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Question(models.Model):
     content = models.TextField()
+
+    def __unicode__(self):
+        return self.content
 
 
 class Answer(models.TextField):
     content = models.TextField()
     question_id = models.ForeignKey(Question)
+
+    def __unicode__(self):
+        return self.content
