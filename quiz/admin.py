103d6ccef628d34b85b5451b56506025fb4c9e30
paul <Semenov_PV@krsk.irgups.ru>
2014-05-22 16:40:03 +0800

    Мелкие правки в моделях. Мелкие правки в админке.

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from quiz.models import Unit, Staff, Answer, Question
+from django.forms import ModelForm, Textarea
 
 # Register your models here.
 
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['content']}),
+    ]
+    inlines = [AnswerInline]
+
 admin.site.register(Unit)
 admin.site.register(Staff)
-admin.site.register(Answer)
-admin.site.register(Question)
+#admin.site.register(Answer)
+admin.site.register(Question, QuestionAdmin)
