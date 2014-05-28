f928a80c4d892098d662efbee1d186c08171f845
paul <Semenov_PV@krsk.irgups.ru>
2014-05-28 16:02:37 +0800

    Изменения в шаблонах

--- quiz/views.py
+++ quiz/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from models import Unit
+from models import Unit, Staff
 
 # Create your views here.
 
 
 def index(request):
-    unit_list = Unit.objects.all()
-    context = {'unit_list': unit_list}
-    return render(request, 'quiz/unit.html', context)
+    unit_list = Unit.objects.order_by("name")
+    staff_list = {}
+    for unit in unit_list:
+        staff_list[unit.name] = Staff.objects.filter(unit_id=unit.id)
+    context = {'staff_list': staff_list}
+    return render(request, 'quiz/index.html', context)
