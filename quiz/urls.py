716d8f13f11c439171f407b4ce4b3477ecc24eaa
paul <Semenov_PV@krsk.irgups.ru>
2014-05-30 14:54:11 +0800

    Добавлен вью quiz

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,5 +4,5 @@ from quiz import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    #url(r'unit/', views.index, name='index'),
+    url(r'^t/(?P<staff_id>\d+)/$', views.quiz, name='quiz'),
 )
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from models import Unit, Staff
 
@@ -13,3 +13,17 @@ def index(request):
         staff_list[unit.name] = Staff.objects.filter(unit_id=unit.id)
     context = {'staff_list': staff_list}
     return render(request, 'quiz/index.html', context)
+
+
+def quiz(request, staff_id):
+    """
+    try:
+        staff = Staff.objects.get(pk=staff_id)
+    except Staff.DoesNotExist:
+        raise Http404
+    """
+    staff = get_object_or_404(Staff, pk=staff_id)
+    if staff.available == True:
+        return render(request, 'quiz/quiz.html', {'staff': staff})
+    else:
+        raise Http404
