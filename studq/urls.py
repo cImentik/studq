8b96b79a60e4d0d92e3481091ab5961c529c85d4
paul <Semenov_PV@krsk.irgups.ru>
2014-06-04 12:49:02 +0800

    Правки в urls

--- studq/urls.py
+++ studq/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^quiz/', include('quiz.urls')),
-    url(r'^t/', include('quiz.urls')),
+    #url(r'^quiz/', include('quiz.urls')),
+    #url(r'^t/', include('quiz.urls')),
     url(r'^', include('quiz.urls')),
 )
