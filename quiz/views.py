8af7aee0c17e1235a7712bd1c7b7acab3333388e
paul <Semenov_PV@krsk.irgups.ru>
2015-01-22 15:27:42 +0700

    закончена реализация логики тестирования

--- quiz/views.py
+++ quiz/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.context_processors import csrf
 import json
 from django.db import connection
+from django.contrib.auth import logout
 
 from .forms import ContactForm, SimpleForm, CurrentForm
 
@@ -19,29 +20,6 @@ def index(request):
         staff_list[unit.name] = Staff.objects.filter(unit_id=unit.id)
     context = {'staff_list': staff_list}
     return render(request, 'quiz/index.html', context)
-
-
-def quiz(request, staff_id, page_number=1):
-    # assert False
-    # #TODO: Ограничение на количество страниц
-    staff = get_object_or_404(Staff, pk=staff_id)
-    if staff.available:
-        content = []
-        question = Question.objects.all()
-        answers = Answer.objects.all()
-        an = [] #answers tuple
-        for q in question:
-            pages = {'question': q.content,
-                     'answers': answers, }
-            content.append(pages)
-        #current_page = Paginator(content, 1)
-        args = {'staff': staff,
-                #'quiz': squiz,
-                # 'content': current_page.page(page_number)#
-                }
-        return render(request, 'quiz/quiz.html', args)
-    else:
-        raise Http404
 
 
 def forms(request, q_id):
@@ -118,7 +96,7 @@ def ajax_test2(request):
     return HttpResponse(context)
 
 
-def mform(request, staff_id, question_id=1):
+def quiz(request, staff_id, question_id=1):
     if not request.session.exists(request.session.session_key):
         request.session.create()
     s = request.session.session_key
@@ -144,8 +122,10 @@ def mform(request, staff_id, question_id=1):
             if mform.is_valid():
                 #если форма заполнена и корректна
                 mform.save()
-                #questions = Question.objects.filter(question_id=question_id)
-                return redirect('mform', staff_id=staff_id, question_id=questions_ids[0])
+                if (int(question_id) in questions_ids) and (len(questions_ids) > 1):
+                    questions_ids = list(questions_ids)
+                    questions_ids.remove(int(question_id))
+                return redirect('quiz', staff_id=staff_id, question_id=questions_ids[0])
             else:
                 #если форма не заполнена или не корректна
                 select_error = True
@@ -158,14 +138,29 @@ def mform(request, staff_id, question_id=1):
     content['mform'] = mform
     content['con'] = connection.queries
     content['staff_name'] = staff.name
+    content['staff_id'] = staff.id
     content['select_error'] = select_error
     content['questions'] = questions_ids
     content['currents_ids'] = currents_ids
     content['end'] = end
 
-    return render(request, 'quiz/mform.html', content)
+    return render_to_response('quiz/mform.html', content)
 
 
-def end(request, staff_id):
-    context = {}
-    return render(request, 'quiz/end.html', context)
+def end(request):
+    logout(request)
+    response = redirect('/')
+    return response
+
+
+def reports(request):
+    unit_list = Unit.objects.order_by("name")
+    staff_list = {}
+    for unit in unit_list:
+        staff_list[unit.name] = Staff.objects.filter(unit_id=unit.id).order_by("name")
+    content = {'staff_list': staff_list}
+    return render_to_response('quiz/reports.html', content)
+
+
+def report(request, staff_id):
+    return HttpResponse("Отчёт для преподователя %s" % staff_id)
