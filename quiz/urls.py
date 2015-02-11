8a3fc811e776981fa5de8b68d6b816b085c2cb53
paul <Semenov_PV@krsk.irgups.ru>
2015-02-11 11:33:26 +0700

    исправлен роутинг

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,12 +3,7 @@ from django.conf.urls import patterns, url
 from quiz import views
 
 urlpatterns = patterns('',
-    url(r'^forms/(?P<q_id>\d+)/$', views.forms, name='forms'),
-    url(r'^form/$', views.form, name='form'),
-    url(r'^jform/$', views.jform, name='jform'),
     url(r'^$', views.index, name='index'),
-    url(r'^ajax/test/$', views.ajax_test, name='ajax_test'),
-    url(r'^ajax/test2/$', views.ajax_test2, name='ajax_test2'),
     url(r'^t/(?P<staff_id>\d+)/(?P<question_id>\d+)/$', views.quiz, name='quiz'),
     url(r'^end/$', views.end, name='end'),
     url(r'^reports/$', views.reports, name='reports'),
